6a9fe45bd9042a70388bcabc40ab62ced38abddd
Jordan Miller <peptides@benchscience.org>
2009-09-23 16:25:15 -0400

    added more copyright info

--- Controller.h
+++ Controller.h
@@ -1,8 +1,4 @@
 //
-//  Controller.h
-//  NSPopUpButtonBindings
-//
-//  Created by Kevin Wojniak on 1/27/08.
 //  Copyright 2009 Jordan Miller, Hive76. All rights reserved.
 //
 
--- main.m
+++ main.m
@@ -1,8 +1,5 @@
 //
 //  main.m
-//  NSPopUpButtonBindings
-//
-//  Created by Kevin Wojniak on 1/27/08.
 //  Copyright Jordan Miller, Hive76 2009. All rights reserved.
 //
 
